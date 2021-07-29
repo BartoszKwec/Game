609e5b4dc6a8a45fdfdc157a515f587c1e086efa
barto <bartosz.kwec@op.pl>
2021-07-29 18:12:32 +0200

    2- podstawowa gra

--- src/pl/kwec/Main.java
+++ src/pl/kwec/Main.java
@@ -1,8 +1,27 @@
 package pl.kwec;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello ");
+
+        Random r = new Random();
+        int player, computer;
+
+
+        do {
+            System.out.println("---------------------");
+            computer=r.nextInt(6)+1;
+            System.out.println("Komputer: "+computer);
+            player = r.nextInt(6)+1;
+            System.out.println("Gracz: "+player);
+            System.out.println("---------------------");
+            if(player != computer) System.out.println("Pudło!");
+            else System.out.println("Udało się!");
+
+
+        }while(player != computer);
     }
 }
